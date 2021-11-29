a0724b8a17aa7eb2aa787e75a12957a6f983f923
Martin-Kennedy <74757431+Martin-Kennedy@users.noreply.github.com>
2021-11-28 23:28:27 -0500

    ++

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/navigation/header';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 import Hero from '../components/hero/hero'
 import { Container, Row, Col } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
